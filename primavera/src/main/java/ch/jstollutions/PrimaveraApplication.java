85dafc1edac47822cbffccaedbfc3b8c1d95aaba
Ruben Stoll <rubenstoll@gmail.com>
2016-02-16 22:33:28 +0100

    spring boot app

--- primavera/src/main/java/ch/jstollutions/PrimaveraApplication.java
+++ primavera/src/main/java/ch/jstollutions/PrimaveraApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrimaveraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrimaveraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PrimaveraApplication.class, args);
+    }
 }
--- primavera/src/main/java/com/tutorialspoint/MainApp.java
+++ primavera/src/main/java/com/tutorialspoint/MainApp.java
@@ -1,15 +1,19 @@
 package com.tutorialspoint;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by tk3sy on 16.02.2016.
  */
-public class MainApp {
+public class
+MainApp {
 
-  public static void main(String[] args) {
-    ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-    HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 
-    obj.getMessage();
-  }
+        obj.getMessage();
+    }
 }
