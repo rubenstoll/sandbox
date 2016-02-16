263c91ab96c45cf31fc11c79eb37e2006d4a7371
Ruben Stoll <rubenstoll@gmail.com>
2016-02-16 22:34:50 +0100

    spring boot app

--- primavera/src/test/java/ch/jstollutions/PrimaveraApplicationTests.java
+++ primavera/src/test/java/ch/jstollutions/PrimaveraApplicationTests.java
@@ -1,16 +1,14 @@
 package ch.jstollutions;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PrimaveraApplication.class)
 public class PrimaveraApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
