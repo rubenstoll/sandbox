6a62e8a2d99abfa420a05e86f9c85d760c5b0332
tk3sy <ruben.stoll@six-group.com>
2016-02-17 18:22:13 +0100

    added spring tutorialspoint hello world

--- primavera/src/main/java/com/tutorialspoint/MainApp.java
+++ primavera/src/main/java/com/tutorialspoint/MainApp.java
@@ -2,6 +2,7 @@ package com.tutorialspoint;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 /**
  * Created by tk3sy on 16.02.2016.
@@ -12,7 +13,9 @@ MainApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+//        ApplicationContext context = new GenericXmlApplicationContext("beans.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld", HelloWorld.class);
 
         obj.getMessage();
     }
