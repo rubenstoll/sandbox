21958ea864f93176791cfb01a1d3a2c79fc4cd8c
tk3sy <ruben.stoll@six-group.com>
2017-02-14 15:39:09 +0100

    template pattern example java code geeks

---
+++ core-cookbook/src/main/java/patterns/templatemethod/AndroidCompiler.java
@@ -0,0 +1,14 @@
+package patterns.templatemethod;
+
+/**
+ */
+public class AndroidCompiler extends CrossCompiler {
+
+  protected void collectSource() {
+    // anything specific to this class
+  }
+
+  protected void compileToTarget() {
+    // android specific compilation
+  }
+}
