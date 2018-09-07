24dbae41129d2952c84640eed7d085d6ccaa9664
tk3sy <ruben.stoll@six-group.com>
2018-09-07 12:16:39 +0200

    updated tool commands

--- core-cookbook/src/main/java/interfaces/oracle/Direction.java
+++
@@ -1,4 +0,0 @@
-package interfaces.oracle;
-
-public class Direction {
-}
--- core-cookbook/src/main/java/principles/dzone/solid/SolidAsARock.java
+++
@@ -1,5 +0,0 @@
-package principles.dzone.solid;
-
-//https://dzone.com/articles/the-solid-principles-in-real-life
-public class SolidAsARock {
-}
