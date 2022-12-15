cd09b55f4a1e818082542437f2a685756e9526c7
sru <ruben.stoll@finnova.com>
2022-12-15 19:18:27 +0100

    NO-ISSUE - unsupported character removal

--- hackatoo/src/main/java/ch/jstollutions/raiffeisen/Profil.java
+++ hackatoo/src/main/java/ch/jstollutions/raiffeisen/Profil.java
@@ -6,7 +6,7 @@ package ch.jstollutions.raiffeisen;
 -  Solide Kenntnisse im Bereich Java/JEE/Spring Boot, Spring Batch, JPA, Hibernate und Hazelcast, Eclipse Scout, JSP, JSF, SQL, XML, CSS und (X)HTML
 -  Solide Kenntnisse von WebServices und Client-Server-Architekturen
 -  Praxis Erfahrungen mit OpenShift, Docker
--  Solide Kenntnisse von Continuous Integration und Delivery mit Jenkins, Gradle und Apache Maven f�r alle Software-L�sungen
+-  Solide Kenntnisse von Continuous Integration und Delivery mit Jenkins, Gradle und Apache Maven fur alle Software-Losungen
 -  Ausgewiesene Praxiserfahrung im Finanzumfeld (Bank oder Versicherung)
 */
 public class Profil {
